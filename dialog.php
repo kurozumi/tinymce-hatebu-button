e5487100b9dc193df06bc667811794e0d3a3e7f9
kurozumi <kurozumi@kurozumi.local>
2016-03-13 13:59:20 +0900

    バグ改修

--- dialog.php
+++ dialog.php
@@ -24,28 +24,6 @@ $feed = simplexml_load_string($feed);
 <html>
 	<head>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-		<script type="text/javascript" src="../../../wp-includes/js/tinymce/tiny_mce_popup.js"></script>
-		<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
-		<script type="text/javascript">
-            jQuery(document).ready(function () {
-                jQuery("body#hatebu button").click(function () {
-                    elem = jQuery(this).parents(".well");
-                    insert(elem);
-                });
-
-                function insert(elem) {
-                    tinyMCEPopup.execCommand('mceInsertContent', false, $("h4", elem).text() + "<br /><br /><br />");
-                    tinyMCEPopup.execCommand('mceInsertContent', false, $("button", elem).data("url"));
-
-                    // Refocus in window
-                    if (tinyMCEPopup.isWindow)
-                        window.focus();
-
-                    tinyMCEPopup.editor.focus();
-                    tinyMCEPopup.close();
-                }
-            });
-		</script>
 	</head>
 	<body id="hatebu">
 		<div class="container-fluid" style="padding:15px;">
@@ -58,9 +36,21 @@ $feed = simplexml_load_string($feed);
 				<div class="well">
 					<h4><a href="<?php echo $item->link; ?>" target="_blank"><?php echo $item->title; ?></a></h4>
 					<p><?php echo $item->description; ?></p>
-					<div class="text-right"><button class="btn btn-primary" data-url="<?php echo $item->link; ?>">挿入</button></div>
+					<div class="text-right"><button class="btn btn-primary" data-url="<?php echo $item->link; ?>" onClick="insert(this)">挿入</button></div>
 				</div>
 			<?php endforeach; ?>
 		</div>
+		<script type="text/javascript">
+            function insert(button) {
+				var args = top.tinymce.activeEditor.windowManager.getParams();
+				var $ = args['jquery'];
+
+				var elem = $(button).parents(".well");
+				
+				var text = $("h4", elem).text();
+				var url = $("button", elem).data("url");
+				top.tinymce.activeEditor.execCommand('mceInsertContent', false,  '<a href="' + url + '">' + text + '</a>');
+            }
+		</script>
 	</body>
 </html>
--- plugin.js
+++ plugin.js
@@ -1,34 +1,35 @@
 (function ($) {
-    tinymce.create('tinymce.plugins.TinyMCEHatebuButton', {
-        init: function (ed, url) {
-
-            ed.addButton('tinymce_hatebu_button', {
-                tooltip: "はてなブックマーク",
-                cmd: 'dialog',
-                image: url + '/images/icon.png'
-            });
-
-            ed.addCommand('dialog', function () {
-                ed.windowManager.open({
-                    title: 'はてなブックマーク',
-                    file: url + '/dialog.php',
-                    width: 508,
-                    height: 308,
-                    inline: 1,
-                    buttons: [{
-                            text: "Cancel",
-                            id: "cancel",
-                            class: "cancel",
-                            onclick: "close"
-                        }]
-                }, {
-                    plugin_url: url
-                });
-            });
-        }
-    });
 
     // Register plugin
-    tinymce.PluginManager.add('tinymce_hatebu_button', tinymce.plugins.TinyMCEHatebuButton);
+    tinymce.PluginManager.add('tinymce_hatebu_button', function (editor, url) {
+        
+        function showDialog() {
+            var win = editor.windowManager.open({
+                title: "はてなブックマーク",
+                file: url + '/dialog.php',
+                width: 600,
+                height: 300,
+                inline: 1,
+                buttons: [{
+                        text: "Close",
+                        id: "close",
+                        class: "close",
+                        onclick: "close"
+                }]
+            }, {
+                plugin_url: url,
+                jquery: $
+            });
+        }
+
+        editor.addCommand("dialog", showDialog);
+
+        editor.addButton('tinymce_hatebu_button', {
+            image: url + '/images/icon.png',
+            tooltip: 'はてなブックマーク',
+            onclick: showDialog
+        });
+
+    });
 
 })(jQuery);
